e1ea1508797195f1be2ded54c2dacd5d46eba990
T.Blount <tblountcs@gmail.com>
2021-03-29 16:12:05 -0400

    mixed lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -11,3 +11,6 @@ print("The second item in the list (port): " + str(my_list[1]) )
 
 # display the third item in the list
 print("The last item in the list (state): " + my_list[2] )
+
+
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
